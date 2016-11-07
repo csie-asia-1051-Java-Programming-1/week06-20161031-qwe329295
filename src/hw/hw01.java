c81e9a2bb1d6e6733f39db5cec363fb831b60285
蕭佑誠 <105021028@live.asia.edu.tw>
2016-11-07 23:15:16 +0800

    hw繳交

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,22 @@
 package hw;
-
+import java.util.*;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		double n =scn.nextDouble();
+		int m =scn.nextInt();
+		fun(n,m);
+		
 
 	}
-
+public static void fun(double n,int m){
+	if(m==1){
+		n=(n-32)*(5d/9d);
+	}
+	if(m==2){
+		n=n*9d/5d+32;
+	}System.out.print(n);
+}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,23 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int m= scn.nextInt();
+		double n =scn.nextDouble();
+		
+		fun(n,m);
 
 	}
-
+public static void fun(double n , int m){
+	if(m==1){
+		n=((n-170)*(0.6d))+62;
+	}
+	if(m==2){
+		n=((n-158)*(0.5d))+52;
+	}
+	System.out.print(n);
+}
 }
